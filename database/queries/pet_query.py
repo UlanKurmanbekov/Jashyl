b16ea518987602f1d54e051a158dd3243864c415
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-08-15 19:20:16 +0600

    add create and select query for pets model

--- database/queries/pet_query.py
+++ database/queries/pet_query.py
@@ -1,1 +1,16 @@
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+from database.models.pet import Pet
+
+
+async def create_pet(session: AsyncSession, telegram_id: int):
+    obj = Pet(telegram_id=telegram_id)
+    session.add(obj)
+    await session.commit()
+
+
+async def get_pet(session: AsyncSession, telegram_id: int) -> Pet:
+    query = select(Pet).where(Pet.telegram_id == telegram_id)
+    result = await session.execute(query)
+    return result.scalar()
 
