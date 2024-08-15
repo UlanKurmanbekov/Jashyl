2f0a1194d19c93ea959f9b61feb27720146ea573
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-08-15 16:42:29 +0600

    add int id pk and date time mixins

--- database/models/achievement.py
+++ database/models/achievement.py
@@ -1,15 +1,16 @@
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database.models.base import Base
-from database.mixins.base_mixin import BaseMixin
+from database.mixins.intidpk_mixin import IntIdPKMixin
+from database.mixins.date_time_mixin import DateTimeMixin
 
 
-class AchievementType(BaseMixin, Base):
+class AchievementType(IntIdPKMixin, DateTimeMixin, Base):
     name: Mapped[str] = mapped_column()
     achievements: Mapped[list['Achievement']] = relationship(back_populates='achievement_type')
 
 
-class Achievement(BaseMixin, Base):
+class Achievement(IntIdPKMixin, DateTimeMixin, Base):
     description: Mapped[str]
     achievement_type_id: Mapped[int] = mapped_column(ForeignKey('achievement_types.id'))
     achievement_type: Mapped['AchievementType'] = relationship(back_populates='achievements')
