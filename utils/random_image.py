68f58b2b25505db3b27bfb9dac248855c4042786
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-08-15 19:23:16 +0600

    update get_random_image

--- utils/random_image.py
+++ utils/random_image.py
@@ -2,8 +2,8 @@ import os
 from random import choice
 
 
-def get_random_image():
-    image_folder = os.path.join(os.path.dirname(__file__), 'pet_images')
-    images = os.listdir(image_folder)
-    random_image = choice(images)
-    return os.path.join(image_folder, random_image)
+def get_random_image() -> str:
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    images_path = os.path.join(project_root, "pet_images")
+    images = [f for f in os.listdir(images_path) if os.path.isfile(os.path.join(images_path, f))]
+    return os.path.join(images_path, choice(images))
