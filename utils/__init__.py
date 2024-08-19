794e11d5ce8e6f776cc303c8d57300c9a241bf94
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-08-19 23:42:30 +0600

    add format timedelta

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,11 @@
 __all__ = (
     'camel_case_to_snake_case',
     'get_random_image',
-    'configure_logging'
+    'configure_logging',
+    'format_timedelta'
 )
 
 from .case_converter import camel_case_to_snake_case
 from .random_image import get_random_image
 from .logger import configure_logging
+from .format_timedelta import format_timedelta
---
+++ utils/format_timedelta.py
@@ -0,0 +1,8 @@
+from datetime import timedelta
+
+
+def format_timedelta(td: timedelta) -> str:
+    total_seconds = int(td.total_seconds())
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+    return f"{hours}ч {minutes}м"
